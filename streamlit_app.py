f97dd64da80c29ae251fe66d5786eccabcc8eac4
gpizzutoa <guero.pizzutoa@gmail.com>
2023-10-21 14:10:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,16 @@
 import openai
 import streamlit as st
+import toml
+
+try:
+    # Try to load the secret from Streamlit's secrets management (for deployed app)
+    openai.api_key = st.secrets["OPENAI_API_KEY"]
+except FileNotFoundError:
+    # If running locally, load the secret from secrets.toml file directly
+    secrets = toml.load("secrets.toml")
+    openai.api_key = secrets["OPENAI_API_KEY"]
 
 st.title("ChatGPT-like clone")
-
-openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
